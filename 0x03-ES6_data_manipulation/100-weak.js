fbb63abf86ba3fa8e49b27ab09fa6427de29818a
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-03-29 13:47:01 +0200

    change of code

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,15 @@
+export const weakMap = new WeakMap();
+
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+  let numCalls = 0;
+
+  if (weakMap.has(endpoint)) {
+    numCalls = weakMap.get(endpoint);
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+
+  weakMap.set(endpoint, numCalls + 1);
+
+  if (numCalls + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
