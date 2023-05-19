c0163583280c34010d1f70db940ed58b2548f247
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-05-19 23:29:08 +0200

    deleted

--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,6 +0,0 @@
-function getPaymentTokenFromAPI(success) {
-  if (success)
-    return Promise.resolve({ data: 'Successful response from the API' });
-}
-  
-module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/README.md
+++
@@ -1,5 +0,0 @@
-# 0x06. Unittests in JS
-
-## Tasks
-
-### 0. Basic test with Mocha and Node assertion library
